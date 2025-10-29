503324433468ca463031edab210a7b9be47a7c56
Marco Fernandes <lelooyq@gmail.com>
2025-10-29 15:50:59 -0300

    Create main.js

---
+++ s/main.js
@@ -0,0 +1,12 @@
+// js/main.js
+
+/**
+ * Ponto de entrada da aplicação.
+ * Este arquivo apenas importa e chama a função de inicialização da SPA.
+ */
+
+import { initApp } from './app.js';
+
+document.addEventListener('DOMContentLoaded', initApp);
+
+console.log("Aplicação Web Dinâmica com JavaScript inicializada!");
